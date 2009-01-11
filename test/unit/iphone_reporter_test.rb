e6e678df0dccc0642352f8ae943221ffcc5cb6db
David Troy <dave@popvox.com>
2009-01-11 12:14:23 -0500

    Expanded iphone reporter test

--- test/unit/iphone_reporter_test.rb
+++ test/unit/iphone_reporter_test.rb
@@ -3,9 +3,12 @@ require File.dirname(__FILE__) + '/../test_helper'
 class IphoneReporterTest < ActiveSupport::TestCase
   def test_iphone_reporter_creation
     reporter = IphoneReporter.create("uniqueid"=>"E8F8D731-648C-5A07-81D8-5C4BA6CDFE45", "zipcode"=>"20171", "firstname"=>"Sze", "lastname"=>"Wong", "email"=>"swong@zerionconsulting.com")
-    assert_match /20171/, reporter.profile_location
+    assert_equal '20171', reporter.profile_location
     assert_nil reporter.followers_count
+    assert_equal "Sze Wong", reporter.name
+    assert_equal "swong@zerionconsulting.com", reporter.screen_name
     assert_match /iPhone App$/, reporter.source_name
+    assert_equal "E8F8D731-648C-5A07-81D8-5C4BA6CDFE45", reporter.uniqueid
     text_report = reporter.text_reports.create("latlon"=>"37.332,-122.031:100", "body"=>"Text again", "title"=>"Test text again")
     assert_equal 100, text_report.location_accuracy
     assert_equal 37.332, text_report.location.latitude
