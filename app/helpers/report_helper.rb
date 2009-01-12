022fd94745ac152672d64f3e74d98c082b47eb93
David Troy <dave@popvox.com>
2009-01-12 17:22:24 -0500

    Added photo link to media helper

--- app/helpers/report_helper.rb
+++ app/helpers/report_helper.rb
@@ -78,7 +78,7 @@ module ReportHelper
     when /AudioReport/
       return "<embed src='#{report.url}' width='100' height='20' AUTOPLAY='false'/>"
     when /PhotoReport/
-      return "<img src='#{report.url}' width='180'/>"
+      report.link_url ? "<a href='#{report.link_url}' class='imageLink'><img src='#{report.url}' width='180'/></a>" : "<img src='#{report.url}' width='180'/>"
     else
       return ""
     end
