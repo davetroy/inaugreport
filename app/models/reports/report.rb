2265428259b86fd5ad1eab0c913f67d6d262f0d8
David Troy <dave@popvox.com>
2009-01-14 15:59:39 -0500

    Restore before_save callback

--- app/models/reports/report.rb
+++ app/models/reports/report.rb
@@ -19,7 +19,7 @@ class Report < ActiveRecord::Base
   before_create :detect_location
   # check_uniqueid must be AFTER create because otherwise it doesn't have an ID
   after_create :check_uniqueid, :assign_filters, :auto_review
-  #before_save { self.location_id = self.reporter.location_id if self.location_id.nil? }
+  before_save { |record| record.location_id = record.reporter.location_id if record.location_id.nil? }
   
   named_scope :with_location, :conditions => 'location_id IS NOT NULL'
   named_scope :with_score, :conditions => 'score IS NOT NULL'
