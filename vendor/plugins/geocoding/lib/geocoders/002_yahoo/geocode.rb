f427e46585e7dda060362d052feb265a9e0ccc34
David Troy <dave@popvox.com>
2009-01-18 13:14:15 -0500

    Tune Yahoo Geocoder to deal with IP addresses

--- vendor/plugins/geocoding/lib/geocoders/002_yahoo/geocode.rb
+++ vendor/plugins/geocoding/lib/geocoders/002_yahoo/geocode.rb
@@ -12,15 +12,15 @@ module Geo
       pstring = params.map { |k,v| "#{k}=#{URI.encode(v)}" }.join('&')
       url = "http://api.local.yahoo.com/MapsService/V1/geocode?#{pstring}"
       loc = ExtractableHash.new.merge(Hash.from_xml(open(url).read))
-      return nil unless loc.extract('ResultSet City')
+      return nil unless loc.extract('ResultSet Result City')
       
       point = Point.from_x_y(loc.extract('ResultSet Result Longitude').to_f, loc.extract('ResultSet Result Latitude').to_f)
       loc = loc.transform( :thoroughfare => 'ResultSet Result Address',
-        :country_code => 'ResultSet Result Country',
         :administrative_area => 'ResultSet Result State',
         :locality => 'ResultSet Result City',
-        :postal_code => 'ResultSet Result Zip')
-      loc[:address] = "#{loc[:thoroughfare]}, #{loc[:locality]}, #{loc[:postal_code]} #{loc[:country_code]}"
+        :postal_code => 'ResultSet Result Zip',
+        :country_code => 'ResultSet Result Country')
+      loc[:address] = %w(thoroughfare locality administrative_area postal_code country_code).map { |f| loc[f.to_sym] }.compact.join(', ')
       loc.merge(:point => point, :geo_source_id => 2)
       
     rescue
