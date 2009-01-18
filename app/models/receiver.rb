49cce9ab57cd1f1710c128faf3407c0394cb85d8
David Troy <dave@popvox.com>
2009-01-17 22:35:39 -0500

    Fix issue with single part emails

--- app/models/receiver.rb
+++ app/models/receiver.rb
@@ -6,25 +6,27 @@ class Receiver < ActionMailer::Base
                   :name => email.friendly_from }
     reporter = EmailReporter.update_or_create(user_info)
 
-    if email.parts
-      parent_report_id = nil
-      email.each_part do |part|
-        report_info = { :title => email.subject, :parent_report_id => parent_report_id }
-        case part.content_type
-          when /text/
-            report = reporter.text_reports.create(report_info.merge(:body => part.unquoted_body))
-          when /image/
-            report = reporter.photo_reports.create(report_info)
-            save_part(report, part, '/photos')
-          when /audio/
-            report = reporter.audio_reports.create(report_info)
-            save_part(report, part, '/audio')
-        end
-        parent_report_id ||= report.id
+    if email.parts.size.zero?
+      reporter.text_reports.create(:title => email.subject, :body => email.body)
+      return true
+    end
+    
+    parent_report_id = nil
+    email.each_part do |part|
+      report_info = { :title => email.subject, :parent_report_id => parent_report_id }
+      case part.content_type
+        when /text/
+          report = reporter.text_reports.create(report_info.merge(:body => part.unquoted_body))
+        when /image/
+          report = reporter.photo_reports.create(report_info)
+          save_part(report, part, '/photos')
+        when /audio/
+          report = reporter.audio_reports.create(report_info)
+          save_part(report, part, '/audio')
+        else
+          report = reporter.text_reports.create(report_info.merge(:body => part.unquoted_body))
       end
-    else
-      report_info = { :title => email.subject, :body => email.body }
-      report = reporter.text_reports.create(report_info)
+      parent_report_id ||= report.id
     end
   end
 
--- test/unit/receiver_test.rb
+++ test/unit/receiver_test.rb
@@ -3,12 +3,18 @@ require 'test_helper'
 class ReceiverTest < ActionMailer::TestCase
   FIXTURES_PATH = File.dirname(__FILE__) + '/../fixtures/receiver'
 
-  test "report is received properly" do
+  test "multipart report is received properly" do
     email_text = File.read("#{FIXTURES_PATH}/taylor_swift")
     assert_match /Taylor-Swift/, email_text
     Receiver.receive(email_text)
-    r = Report.find(:all)
-    p r
+    reports = Report.find(:all)
+    assert_equal 3, reports.size
   end
 
+  test "single part report is received properly" do
+    email_text = File.read("#{FIXTURES_PATH}/single_part_email")
+    Receiver.receive(email_text)
+    report = Report.find(:first)
+    assert_match /single part email/, report.body
+  end
 end
