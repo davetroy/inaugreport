5637044902b67817fe28a42cf8814f90420a2949
David Troy <dave@popvox.com>
2009-01-17 23:54:45 -0500

    Tune email settings; set icon

--- app/models/reporters/email_reporter.rb
+++ app/models/reporters/email_reporter.rb
@@ -3,6 +3,6 @@ class EmailReporter < Reporter
   def source_name; "Email"; end
   def source; "Email"; end
   def icon; "/images/email_icon.jpg"; end
-  def display_name; screen_name || "Email User"; end
+  def display_name; name || "Email User"; end
   
 end
