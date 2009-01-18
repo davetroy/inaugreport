fa411e5dda05a4bd8ae2d021c46212dbbb035125
David Troy <dave@popvox.com>
2009-01-17 23:47:44 -0500

    Fix Maildir path

--- lib/daemons/poll_email.rb
+++ lib/daemons/poll_email.rb
@@ -7,7 +7,7 @@ ENV["RAILS_ENV"] ||= defined?(Daemons) ? 'production' : 'development'
 
 require File.dirname(__FILE__) + "/../../config/environment"
 
-MAILSPOOL = RAILS_ENV=='production' ? "/home/inaugreport/Maildir/cur" : File.dirname(__FILE__) + "/../../test/fixtures/receiver"
+MAILSPOOL = RAILS_ENV=='production' ? "/home/inaugreport/Maildir/new" : File.dirname(__FILE__) + "/../../test/fixtures/receiver"
 
 $running = true; Signal.trap("TERM") { $running = false }
 
