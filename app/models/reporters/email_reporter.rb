236c646f3e16c959695df37a23fd31f9a9e2ea5a
David Troy <dave@popvox.com>
2009-01-17 23:51:30 -0500

    Fix display_name in email reporter

--- app/models/reporters/email_reporter.rb
+++ app/models/reporters/email_reporter.rb
@@ -3,6 +3,6 @@ class EmailReporter < Reporter
   def source_name; "Email"; end
   def source; "Email"; end
   def icon; "/images/email_icon.jpg"; end
-  def name; screen_name || "Email User"; end
+  def display_name; screen_name || "Email User"; end
   
 end
