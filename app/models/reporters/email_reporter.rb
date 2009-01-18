d0b1d4a549d057bf6dab7a8845d412a5ef6a3691
David Troy <dave@popvox.com>
2009-01-17 22:05:13 -0500

    Clear location hooks from email reporter

--- app/models/reporters/email_reporter.rb
+++ app/models/reporters/email_reporter.rb
@@ -1,13 +1,8 @@
 class EmailReporter < Reporter
-  before_create :initialize_location
 
   def source_name; "Email"; end
   def source; "Email"; end
   def icon; "/images/email_icon.jpg"; end
   def name; screen_name || "Email User"; end
   
-  private
-  def initialize_location
-    self.location = Location.geocode(self.profile_location) if self.profile_location.is_a?(String) && !self.profile_location[/\d{10}/]
-  end
 end
